335859bcaabc48f7ef7057e67690e966eed492bd
MrKeith99 <kvalentincardenas@hotmail.com>
2023-11-24 14:17:26 +0900

    [UPD] change comments

--- install.sh
+++ install.sh
@@ -4,32 +4,29 @@
 
 echo "╔══╣ Install: RealSense ROS Wrapper (STARTING) ╠══╗"
 
-# 以前のパッケージを削除する
+
+# Delete previous realseanse packages
 dpkg -l | grep "realsense" | cut -d " " -f 3 | xargs -t sudo apt-get -y purge
 
 
-#サーバーの公開鍵の登録
+# Add server key to apt-key
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 
-#リポジトリの登録
+# Add server to list of repositories
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
 sudo apt update
 
-#librealsenseのインストール
+# Install the libraries
 sudo apt-get install -y \
     librealsense2-dkms \
     librealsense2-utils \
     librealsense2-dev \
     librealsense2-dbg
 
-#ddynamic_reconfigureをアップデート
+# Install ROS dependencies
 sudo apt install -y \
-     ros-$ROS_DISTRO-ddynamic-reconfigure
-
-#必要なROSパッケージのインストール
-# sudo apt-get install -y \
-#     ros-$ROS_DISTRO-realsense2-camera
+     ros-$ROS_DISTRO-ddynamic-reconfigure \
+     ros-$ROS_DISTRO-imu-filter-madgwick
 
 
 echo "╚══╣ Install: RealSense ROS Wrapper (FINISHED) ╠══╝"
-echo "ホスト側の端末でudevにデバイス情報を記述してください"
