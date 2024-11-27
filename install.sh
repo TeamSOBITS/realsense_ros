6b5a92c88f796870046a1862347fe5090eee6a11
MrKeith99 <kvalentincardenas@hotmail.com>
2024-11-27 21:50:39 +0900

    [UPD] installer with deb

--- install.sh
+++ install.sh
@@ -5,22 +5,7 @@
 echo "╔══╣ Install: RealSense ROS 2 Wrapper (STARTING) ╠══╗"
 
 # Install librealsense2 debian package from Intel servers
-sudo mkdir -p /etc/apt/keyrings
-curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
-
-sudo apt-get update
-sudo apt install -y \
-   apt-transport-https
-
-echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
-sudo tee /etc/apt/sources.list.d/librealsense.list
-sudo apt-get update
-
-sudo apt-get install -y librealsense2-dkms
-sudo apt-get install -y librealsense2-utils
-
-sudo apt-get install -y librealsense2-dev
-sudo apt-get install -y librealsense2-dbg
+sudo apt install ros-$ROS_DISTRO-librealsense2*
 
 # Install Intel® RealSense™ ROS2 wrapper dependencies
 sudo apt-get update
