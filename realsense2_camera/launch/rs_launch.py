d6dd42c294cc2c2ef92b702d45f4d9cb382be0b6
Y.hanajima <sakuyuu2002@gmail.com>
2025-10-10 19:05:37 +0900

    update README and qos

--- realsense2_camera/launch/rs_launch.py
+++ realsense2_camera/launch/rs_launch.py
@@ -20,9 +20,12 @@ import launch_ros.actions
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 
+camera_name = 'camera'
+robot_name = 'D435i'
 
-configurable_parameters = [{'name': 'camera_name',                  'default': 'camera', 'description': 'camera unique name'},
-                           {'name': 'camera_namespace',             'default': '', 'description': 'namespace for camera'},
+
+configurable_parameters = [{'name': 'camera_name',                  'default': camera_name, 'description': 'camera unique name'},
+                           {'name': 'camera_namespace',             'default': robot_name, 'description': 'namespace for camera'},
                            {'name': 'serial_no',                    'default': "''", 'description': 'choose device by serial number'},
                            {'name': 'usb_port_id',                  'default': "''", 'description': 'choose device by usb port id'},
                            {'name': 'device_type',                  'default': "''", 'description': 'choose device by type'},
